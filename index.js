caceaa032f3eb09419adb6775f833102f181e23d
Chinenye Schola Chukwuelue <scholachi23@gmail.com>
2021-11-29 10:39:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+            name: '',
+        };
+    },
+
+    methods:{
+        setName(event, lastname){
+            this.name = event.target.value + ' '+ lastname;
+            
+        },
+        add(num) {
+           this.counter = this.counter + num;
+        },
+        remove(num){
+            this.counter = this.counter- num;
+        }
+
+    }
+
+});
+app.mount('#events');
